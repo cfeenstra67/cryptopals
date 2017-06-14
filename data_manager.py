d164ebda77d7fdf707736744e76b402310d68ca9
cfeenstra67 <cameron.l.feenstra@gmail.com>
2017-06-14 16:49:11 -0400

    added model

---
+++ data_manager.py
@@ -0,0 +1,61 @@
+#!/usr/bin/local/python3
+
+import sqlite3
+from txt_learn import abs_path, DATA_FOLDER, DATABASE_DEST, MODEL_DEST, DATAFRAME_DEST, wrap_str
+
+with sqlite3.connect(abs_path(DATABASE_DEST)) as conn:
+	# Managing the Databse
+	curs = conn.cursor()
+	
+	def __maketable(cursor):
+		cursor.execute("""
+		CREATE TABLE IF NOT EXISTS all_data (
+			id	INTEGER	PRIMARY KEY AUTOINCREMENT,
+			name TEXT,
+			english INT,
+			content TEXT
+		)
+		""")
+	
+	__maketable(curs)
+
+	def run_query(query, vals=None, quiet=False): 
+		try: 
+			if vals: curs.execute(query, vals)
+			else: curs.execute(query)
+		except: 
+			if not quiet: print('Query Failed: %s' % query)
+
+	def add_text_samples(*args, quiet=False):
+		"""
+		Adds rows w/ values provided in each argument, 3-tuple formatted as (name, english, content)
+		"""
+		for name, english, content in args:
+			run_query("""
+				INSERT INTO all_data (name, english, content) VALUES (?,?,?)
+				""", (wrap_str(name), int(english), wrap_str(content)), quiet=quiet)
+		conn.commit()
+
+	def add_text_sample(name,english,content, quiet=False):
+		"""
+		Add a single row w/ values provided
+		"""
+		add_text_samples((name,english,content), quiet=quiet)
+
+	def get_all_samples(fields='*', quiet=False):
+		"""
+		Retrieve all rows of the table.  May specify a string to use for as indexes i.e. (name, english, content)
+		"""
+		run_query("""
+			SELECT %s FROM all_data
+			""" % fields, quiet=quiet)
+		return curs.fetchall()
+
+	def clear_all_samples(quiet=False):
+		"""
+		Clear all samples currently held in database
+		"""
+		run_query("""
+			DELETE FROM all_data
+			""", quiet=quiet)
+		conn.commit()
--- txt_learn.py
+++ txt_learn.py
@@ -1,41 +1,122 @@
 #!/usr/local/bin/python3
+"""
+This module assumes the existance of a pickled file containing a pandas dataframe
+at the DATFRAME_DEST.  To generate this file, use the data_generator model
+"""
+DATA_FOLDER = 'data'
+def abs_path(filename): return '%s/%s' % (DATA_FOLDER, filename)
+DATAFRAME_DEST = 'dataframe.pkl'
+MODEL_DEST = 'model.pkl'
+DATABASE_DEST = 'english-text.db'
 
-import sqlite3
+from data_manager import *
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 
 #Utility Functions
 from crypto import m_gen
-def wrap_str(text,wrapper='\''): return ''.join((wrapper,text,wrapper))
+def wrap_str(text,wrapper='`'): return ''.join((wrapper,text,wrapper))
 def unwrap_str(text): return text[1:-1]
+def bounded(num, lower=0, upper=1): return min(upper, max(lower, num))
+def arr_for_string(s, lower=False):
+	arr = np.zeros(264)
+	for char in bytes(s.lower() if lower else s, encoding='utf-8'):
+		arr[char] += 1
+	return arr / np.sum(arr)
+	
+def retrieve_data(dataframe_file=DATAFRAME_DEST):
+	"""
+	Retrieve the X data and Y data from the stored dataframe
+	"""
+	with open(abs_path(dataframe_file), 'rb') as data_file: 
+		df = pickle.loads(data_file.read())
+		y, X = np.transpose(df.as_matrix())
+		def reshaped(mat):
+			mat_s = np.empty((len(mat),len(mat[0])))
+			for i, row in enumerate(mat): mat_s[i] = row
+			return mat_s
+		X_s = reshaped(X)
+		y_s = np.array(y, dtype=int)
+		for i, row in enumerate(X): X_s[i] = row
+		return X_s, y_s
 
-with sqlite3.connect("english-text.db") as conn:
-	# Managing the Databse
-	curs = conn.cursor()
-	curs.execute("""
-		CREATE TABLE IF NOT EXISTS all_data (
-			id	INTEGER	PRIMARY KEY AUTOINCREMENT,
-			name TEXT,
-			english INT,
-			content TEXT
-		)
-		""")
 
-	def run_query(query): curs.execute(query)
+def create_model(X, y, destination_file=MODEL_DEST):
+	"""
+	Create the model based on data given
+	"""
+	clf = LogisticRegression()
+	clf.fit(X, y)
+	with open(destination_file, 'wb+') as model_file: pickle.dump(clf, model_file)
+	return clf
 
-	def add_text_sample(name,english,content):
-		"""
-		Add a row the to table w/ values provided
-		"""
-		run_query("""
-			INSERT INTO all_data (name, english, content) VALUES (%s,%d,%s)
-			""" % (wrap_str(name), int(english), wrap_str(content)))
-		conn.commit()
+def retrieve_model(model_file=MODEL_DEST):
+	"""
+	Retrieve saved model
+	"""
+	with open(model_file, 'rb') as model_file: return pickle.load(model_file)
 
-	def get_all_samples(fields='*'):
-		"""
-		Retrieve all rows of the table.  May specify a string to use for as indexes i.e. (name, english, content)
-		"""
-		run_query("""
-			SELECT %s FROM all_data
-			""" % fields)
-		return curs.fetchall()
+
+
+def predict_string(some_str, model=None):
+	"""
+	Returns the predicted probability
+	"""
+	if model is None:
+		try: model = retrieve_model()
+		except FileNotFoundError: model = create_model()
+	str_arr = arr_for_string(some_str)
+	(n, y), = model.predict_proba(str_arr.reshape(1, -1))
+	return y
+
+def run_test(X, y, mod):
+	"""
+	Tests accuracy of given model against given data.
+	"""
+	correct=[]
+	for X_i, y_i in zip(X, y):
+		y_p = mod.predict(X_i.reshape(1, -1))
+		correct.append(y_p == y_i)
+	c, t = correct.count(True), len(correct)
+
+	print("""
+	Test Complete.
+
+	Correct: %d
+	Total: %d
+	Percent: %.3f
+	""" % (c, t, (c / t) * 100.))
+
+def run_interactive(mod):
+	"""
+	Runs command line program which predicts the probability of input strings being english based on the saved model.
+	"""
+	inp = lambda: input('Predict: ')
+	input_string = inp()
+	while len(input_string):
+		prob = predict_string(input_string, mod)
+		print('Estimated Probability: %.2f' % prob)
+		input_string = inp()
+
+if __name__ == '__main__':
+	import argparse
+
+	X, y = retrieve_data()
+	mod = None
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-t', '--test', action='store_true', help='test model with trained data')
+	parser.add_argument('-i', '--interactive', action='store_true', help='open shell which allows you to predict probabilities of strings being English text')
+	parser.add_argument('-r', '--retrieve-model', action='store_true', help='instead of training model using pickled dataframe, simply retrieve the currently saved model.')
+	args = parser.parse_args()
+
+	if args.retrieve_model: mod = retrieve_model()
+	else: mod = create_model(X, y) 
+	if args.test: run_test(X, y, mod)
+	if args.interactive: run_interactive(mod)
+
+
 
